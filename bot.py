97e49310cfc922b0b0d6f0ec402543f18cdc4e7a
kstonekuan <kingstonkuan@u.nus.edu>
2021-01-31 02:01:40 +0800

    remove comments

--- bot.py
+++ bot.py
@@ -1,20 +1,3 @@
-  
-#!/usr/bin/env python
-# pylint: disable=W0613, C0116
-# type: ignore[union-attr]
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 import csv
 import config
